fe46638f643941e288af4125c8d5e08b280b36f7
divyadave25024 <divyadave25024@gmail.com>
2023-01-02 21:44:35 +0530

    feat : changes

--- src/app/components/form-components/form-wizard/form-wizard.component.ts
+++ src/app/components/form-components/form-wizard/form-wizard.component.ts
@@ -6,11 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-wizard.component.scss'],
 })
 export class FormWizardComponent implements OnInit {
+  stepsArray: any = [
+    {
+      stepper_type: 'horizontal',
+      stepper_id: '001_horizone',
+      stepper_data: [
+        {
+          step_index: 1,
+          step_title: 'Step 1',
+          step_state: 'stepper-active',
+        },
+        {
+          step_index: 2,
+          step_title: 'Step 2',
+          step_state: 'stepper-completed',
+        },
+        {
+          step_index: 3,
+          step_title: 'Step 3',
+          step_state: '',
+        },
+      ],
+    },
+  ];
+
+  stepActive: number = 1;
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  stepSelected(event){
+  stepSelected(event, step_no) {
     console.log('here event is: ', event);
+    this.stepActive = step_no;
   }
 }
